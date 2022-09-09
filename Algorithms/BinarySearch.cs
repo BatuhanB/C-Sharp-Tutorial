ecdf82f7f1ba553f1a2a604c662ef6bd6e4f457c
BatuhanB <balibatuhan19@gmail.com>
2022-09-09 11:10:11 +0300

    movezeroes algorithm question has been added

--- Algorithms/BinarySearch.cs
+++ Algorithms/BinarySearch.cs
@@ -115,6 +115,26 @@ namespace Algorithms
 
         #endregion
 
+        #region MoveZeroes
+        // Given an integer array nums, move all 0's to the end of it 
+        // while maintaining the relative order of the non-zero elements.
+        // Note that you must do this in-place without making a copy of the array.
 
+        // Input: nums = [0,1,0,3,12]            Input: nums = [0]
+        // Output: [1,3,12,0,0]                  Output: [0]
+
+        public int[] MoveZeroes(int[] nums)
+        {
+            for (int i = 0,k = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) continue;
+                (nums[k], nums[i]) = (nums[i], nums[k]);
+                k++;
+            }
+
+            return nums;
+        }
+
+        #endregion
     }
 }
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -11,11 +11,13 @@ namespace Algorithms
             var result = binarySearch.Search(new[] { -1, 2, 3, 5, 12, 24 }, 12);
             Console.WriteLine($"Target Index: {result}");
             Console.WriteLine("******************************");
+
             var result2 = binarySearch.SearchInsert(new[] { 1, 3, 5, 6 }, 6);
             var result3 = binarySearch.SearchInsert(new[] { 1, 3, 5, 6 }, 8);
             Console.WriteLine($"Target Index: {result2}");//3
             Console.WriteLine($"Target Index: {result3}");//4
             Console.WriteLine("******************************");
+
             var result4 = binarySearch.SortedSquaredArray(new[] { -8, -4, -2, 0, 1, 3, 5, 6, 9 });
             Console.WriteLine($"Original Array: [-8, -4, -2, 0, 1, 3, 5, 6, 9 ]");
             Console.Write($"Squared Array Elements: ");
@@ -26,6 +28,18 @@ namespace Algorithms
             }
             
             Console.WriteLine("]");
+            Console.WriteLine("******************************");
+
+            var result5 = binarySearch.MoveZeroes(new[] { 0, 1, 0, 3, 12 });
+            Console.Write($"Sorted Array Elements: ");
+            Console.Write(" [");
+            foreach (var item in result5)
+            {
+                Console.Write($"{item} , ");
+            }
+            
+            Console.WriteLine("]");
+            Console.WriteLine("******************************");
         }
     }
 }
