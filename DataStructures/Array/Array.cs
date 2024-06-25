e9e3a50783089ee3869ad1cc46e4ddfa89a3a871
BatuhanB <balibatuhan19@gmail.com>
2024-06-25 21:09:27 +0300

    Array Constructor Size Arrange

--- DataStructures/Array/Array.cs
+++ DataStructures/Array/Array.cs
@@ -1,14 +1,21 @@
-﻿
-namespace DataStructers.Array;
+﻿namespace DataStructers.Array;
 
 public class Array
 {
     public int Length => InnerArray.Length;
     private Object[] InnerArray { get; set; }
 
+    // Default constructor with a size of 16
     public Array(int size = 16)
     {
+        ArrayHelpers.CheckIfSizeOfArrayValid(size);
         InnerArray = new Object[size];
     }
 
+    // Constructor that takes an array and initializes the internal array with its elements
+    public Array(params Object[] sourceArray) : this(sourceArray?.Length ?? 0)
+    {
+        ArrayHelpers.CheckIfSourceArrayNull(sourceArray);
+        System.Array.Copy(sourceArray,InnerArray,sourceArray.Length);
+    }
 }
---
+++ DataStructures/Array/ArrayHelpers.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructers.Array;
+internal static class ArrayHelpers
+{
+    internal static void CheckIfSizeOfArrayValid(int size)
+    {
+        if (size < 0)
+            throw new ArgumentOutOfRangeException(nameof(size), "Size must be non-negative!");
+    }
+
+    internal static void CheckIfSourceArrayNull(Object[] sourceArray)
+    {
+        if (sourceArray == null)
+            throw new ArgumentNullException(nameof(sourceArray), "Source array cannot be null.");
+    }
+}
