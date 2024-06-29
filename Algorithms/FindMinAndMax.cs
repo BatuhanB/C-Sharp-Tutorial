6990b0f424c6c749706128b2bdb4166fe723f198
BatuhanB <balibatuhan19@gmail.com>
2024-06-29 19:45:44 +0300

    SortArray and MinMax Algorithms Added

---
+++ Algorithms/FindMinAndMax.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Algorithms;
+public static class FindMinAndMax
+{
+    public static void FindMinAndMaxM()
+    {
+        // 3,6,1,9,12,7,24,21,45,32
+        var arr = new List<int>
+        {
+            63,
+            3,
+            6,
+            1,
+            9,
+            12,
+            7,
+            24,
+            21,
+            45,
+            32
+        };
+
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.Count; i++)
+        {
+            if (min > arr[i])
+            {
+                min = arr[i];
+            }
+
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+        }
+
+        Console.WriteLine($"Min => {min} \t Max => {max}");
+    }
+}
---
+++ Algorithms/SortArray.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Algorithms;
+public static class SortArray
+{
+    public static void SortArrayM()
+    {
+        // 3,6,1,9,12,7,24,21,45,32
+        var arr = new List<int>
+        {
+            63,
+            3,
+            6,
+            1,
+            9,
+            12,
+            7,
+            24,
+            21,
+            45,
+            32
+        };
+
+        var sortedArr = new List<int>();
+
+        while (arr.Count > 0)
+        {
+            var target = arr[0];
+            foreach (int i in arr)
+            {
+                if (target > i)
+                {
+                    target = i;
+                }
+            }
+            sortedArr.Add(target);
+            arr.Remove(target);
+        }
+
+        foreach (int i in sortedArr)
+        {
+            Console.WriteLine($"{i}\t");
+        }
+    }
+}
