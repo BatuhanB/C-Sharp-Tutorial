5d31eb93b86700cd9cb9ee3cdbf38b55ba3e905a
BatuhanB <balibatuhan19@gmail.com>
2024-07-14 20:36:31 +0300

    Insertion Sort Algorithm has added

---
+++ InsertionSort/MyInsertionSort.cs
@@ -0,0 +1,20 @@
+﻿namespace InsertionSort;
+public class MyInsertionSort
+{
+    // [12,3,5,2,6,85,23,13]
+    public void Sort(params int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > current)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using InsertionSort;
+
+int[] nums = [12, 3, 5, 2, 6, 85, 23, 13];
+var insertionSort = new MyInsertionSort();
+
+insertionSort.Sort(nums);
+
+foreach (int i in nums)
+{
+    Console.Write($"{i}  ");
+}
