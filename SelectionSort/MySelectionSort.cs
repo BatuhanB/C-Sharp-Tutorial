d3ee33ad14222db5e63d384a4699ef3c2a054eff
BatuhanB <balibatuhan19@gmail.com>
2024-07-14 20:05:51 +0300

    Selection Sort Sorting Algorithm has added

---
+++ SelectionSort/MySelectionSort.cs
@@ -0,0 +1,20 @@
+﻿namespace SelectionSort;
+public class MySelectionSort
+{
+    // [1,3,5,2,6,85,23,12]
+    public void Sort(params int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var minIndex = i;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if(arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            (arr[minIndex], arr[i]) = (arr[i], arr[minIndex]);
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using SelectionSort;
+
+int[] arr = [1,3,5,2,6,85,23,12];
+var selectionSort = new MySelectionSort();
+
+selectionSort.Sort(arr);
+
+foreach (int i in arr)
+{
+    Console.Write($"{i}  ");
+}
+Console.ReadKey();
