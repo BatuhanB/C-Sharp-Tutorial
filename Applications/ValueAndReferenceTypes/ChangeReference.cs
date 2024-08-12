e22c9dd68a85e670a97392d52e8c3dfbf5506179
BatuhanB <balibatuhan19@gmail.com>
2024-08-12 13:03:10 +0300

    Reference type change over method using ref and out keywords tests

---
+++ Applications/ValueAndReferenceTypes/ChangeReference.cs
@@ -0,0 +1,28 @@
+﻿namespace ValueAndReferenceTypes;
+public static class ChangeReference
+{
+    public static Product ChangeByReference(ref Product itemRef)
+    {
+        return itemRef = new Product("Changed Ref", 12345);
+    }
+    public static Product ChangeByReferenceWithOutRef(Product itemRef)
+    {
+        return itemRef = new Product("Changed Ref", 12345);
+    }
+    public static Product ChangeByReferenceUsingOut(out Product itemRef)
+    {
+        return itemRef = new Product("Changed Ref", 12345);
+    }
+}
+
+public class Product
+{
+    public Product(string name, int newID)
+    {
+        ItemName = name;
+        ItemID = newID;
+    }
+
+    public string ItemName { get; set; }
+    public int ItemID { get; set; }
+}
---
+++ Tests/TestApplications/ChangeReferenceTests.cs
@@ -0,0 +1,47 @@
+﻿using ValueAndReferenceTypes;
+
+namespace TestApplications;
+public class ChangeReferenceTests
+{
+    [Fact]
+    public void Check_If_Change_Reference_Without_Ref()
+    {
+        //arrange
+        Product product = new("Stapler", 12);
+        var refChangedProduct = ChangeReference.ChangeByReferenceWithOutRef(product);
+
+        //act
+        var isChange = ReferenceEquals(product, refChangedProduct);
+
+        //assert
+        Assert.False(isChange);
+    }
+
+    [Fact]
+    public void Check_If_Change_Reference_Ref_Keyword()
+    {
+        //arrange
+        Product product = new("Stapler", 12);
+        var refChangedProduct = ChangeReference.ChangeByReference(ref product);
+
+        //act
+        var isChange = ReferenceEquals(product, refChangedProduct);
+
+        //assert
+        Assert.True(isChange);
+    }
+
+    [Fact]
+    public void Check_If_Change_Reference_Out_Keyword()
+    {
+        //arrange
+        Product product = new("Stapler", 12);
+        var refChangedProduct = ChangeReference.ChangeByReferenceUsingOut(out product);
+
+        //act
+        var isChange = ReferenceEquals(product, refChangedProduct);
+
+        //assert
+        Assert.True(isChange);
+    }
+}
